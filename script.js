a68edb3776e5fb1c026761aafc7f4e0a23e3b44f
Felicia_V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2024-12-19 15:45:30 +0100

    edit file

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 // G-Version - Split the Nota
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 /*
-
-
+Starta
+    Skapa function splitnota:
+        Läs in värdet för summan i "price" från anvädarens input
+        Läs in värdet för antal personen i "count" från anvädarens input
+        Läs in värdet för dricks i "tip" från anvädarens input
+    
+        Räkna ut värden med "price, count, tip"
+            Räkna ut dricks med tip till tipamount: tipamount = price * tip
+            Räkna ut den totala belooppet per personen: reslutat = (price + tipamount) / count
+            Skriv ut texten: "Varje person ska betala (reslutat) SEK"
+        Om användare fyll inte in information
+            Skriv ut texten: "Vänligen fyll i alla fält"
+        
+        Skriv ut reslutat på sidan
+        Skriv ut resultat på konsole sidan.
+End
 */
 
-
-function summan() {
-    let x = document.getElementById("price").value;
-    let y = document.getElementById("antal").value;
-    let z = document.getElementById("tip").value;
-
-    let summan;
-    if (x, y, z) {
-        a = 1 + z;
-        summan = x * a / y;
+function splitnota() {
+    let price = Number(document.getElementById("price").value);
+    let count = Number(document.getElementById("count").value);
+    let tip = Number(document.getElementById("tip").value);
+    
+    let splitsumman;
+    if (price, count, tip) {
+        tipamount = price * tip;
+        reslutat = (price + tipamount) / count;
+        text = "Varje person ska betala " + reslutat + " SEK";
+    } else {
+        text = "Vänligen fyll i alla fält";
     }
-
-    document.getElementById("split").innerHTML = "Varje person ska betala " + summan + " sek";
+    document.getElementById("split").innerHTML = text;
+    console.log(text);
 }
-
 
 // VG-Version - Lewis Carroll Word Puzzle
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 // Startkoden är 100% frivillig och kan tas bort eller skrivas om
 /*
+
+
 
 play();
 
@@ -36,12 +53,29 @@ function play()
 
 end function
 
+
+
 function isOneLetterApart(wordOne, wordTwo)
-    SET variabel diffCount till 0;
+    set variabel diffCount till 0;
 
     // Här behöver du skriva koden för din funktion
 
     return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
-end function
-
+end function;
 */
+
+let ordbok = ["FOUR", "FOUL", "FOOL", "FOOT", "FORT", "FORE", "FIRE", "FIVE"];
+let answer = prompt("Starta spelet \n\nDu ska gissa orden genom att byta ett bokstav från den starta orden \n\nStarta orden är Four");
+let text = prompt("Nuvarande ord: " + answer + "\n\nSkriver ett ord som skiljer sig med ett bokstav från " + answer + "\n\nDu har gissat gånger");
+let text2 = prompt("Nuvarande ord: " + text + "\n\nSkriver ett ord som skiljer sig med ett bokstav från " + answer + "\n\nDu har gissat gånger");
+
+let matchCount = 0;
+if (firstWord.length !== secondWord.length) {
+    console.log('Båda orden måste innehålla lika många bokstäver');
+}
+for (let i = 0; i < firstWord.length; i++) {
+    if (firstWord[i] === secondWord[i]) {
+        matchCount++;
+    }
+}
+alert(`Det fanns ${matchCount} likheter mellan orden`);
